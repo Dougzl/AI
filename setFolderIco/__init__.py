9f2cee58034022b30791ff121ff8d00a82d0626e
dongz <895180729@qq.com>
2024-07-22 23:35:45 +0800

    init

--- setFolderIco/__init__.py
+++ setFolderIco/__init__.py
@@ -7,7 +7,6 @@ from tkinter import filedialog, messagebox
 import shutil
 import subprocess
 import ctypes
-from ctypes import wintypes
 
 # Set directory as a global variable
 directory = None
@@ -202,24 +201,18 @@ def process_icon(icon):
             messagebox.showerror("Error", f"Failed to create desktop.ini: {e}")
 
 def refresh_explorer():
-    # http://msdn.microsoft.com/en-us/library/ms644950
-    SendMessageTimeout = ctypes.windll.user32.SendMessageTimeoutA
-    SendMessageTimeout.restype = wintypes.LPARAM  # aka LRESULT
-    SendMessageTimeout.argtypes = [wintypes.HWND, wintypes.UINT, wintypes.WPARAM, wintypes.LPARAM,
-                                   wintypes.UINT, wintypes.UINT, ctypes.c_void_p]
+    global directory
+    # 定义必要的常量
+    SHCNE_UPDATEITEM = 0x00000001
+    SHCNF_PATHW = 0x0005
 
-    # http://msdn.microsoft.com/en-us/library/bb762118
-    SHChangeNotify = ctypes.windll.shell32.SHChangeNotify
-    SHChangeNotify.restype = None
-    SHChangeNotify.argtypes = [wintypes.LONG, wintypes.UINT, wintypes.LPCVOID, wintypes.LPCVOID]
-
-    HWND_BROADCAST = 0xFFFF
-    WM_SETTINGCHANGE = 0x001A
-    SMTO_ABORTIFHUNG = 0x0002
-    SHCNE_ASSOCCHANGED = 0x08000000
-
-    SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 0, SMTO_ABORTIFHUNG, 5000, None)
-    SHChangeNotify(SHCNE_ASSOCCHANGED, 0, None, None)
+    # 调用 SHChangeNotify
+    ctypes.windll.shell32.SHChangeNotify(
+        SHCNE_UPDATEITEM,
+        SHCNF_PATHW,
+        directory,
+        0
+    )
 
 # Main application
 def main():
